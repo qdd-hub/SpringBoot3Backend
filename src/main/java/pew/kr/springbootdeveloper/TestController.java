00e52142fd9287619c87e050d80edefec91a1331
parkeunwoo <qkrdmsddn123@gmail.com>
2025-11-11 11:49:59 +0900

    4

---
+++ src/main/java/pew/kr/springbootdeveloper/TestController.java
@@ -0,0 +1,19 @@
+package pew.kr.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+    @Autowired
+    TestService testService;
+
+    @GetMapping("/test")
+    public List<Member> getAllMembers(){
+        List<pew.kr.springbootdeveloper.Member> members = testService.getAllMembers();
+        return members;
+    }
+}
