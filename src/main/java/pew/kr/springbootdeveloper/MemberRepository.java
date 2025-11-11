5c2c5c55520d0369f3ce8be3abefe23aab2cd521
parkeunwoo <qkrdmsddn123@gmail.com>
2025-11-11 13:40:49 +0900

    6

--- src/main/java/pew/kr/springbootdeveloper/MemberRepository.java
+++ src/main/java/pew/kr/springbootdeveloper/MemberRepository.java
@@ -3,6 +3,9 @@ package pew.kr.springbootdeveloper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
 }
---
+++ src/test/java/pew/kr/springbootdeveloper/MemberRepositoryTest.java
@@ -0,0 +1,94 @@
+package pew.kr.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @AfterEach
+    public void cleanUp(){
+        memberRepository.deleteAll();
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getAllMembers(){
+        List<Member> members = memberRepository.findAll();
+
+        assertThat(members.size()).isEqualTo(3);
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getMemberById(){
+
+        Member member = memberRepository.findById(2L).get();
+        assertThat(member.getName()).isEqualTo("B");
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getMemberByName(){
+        Member member = memberRepository.findByName("C").get();
+        assertThat(member.getId()).isEqualTo(3);
+
+    }
+
+    @Test
+    void saveMember(){
+        Member member = new Member(1L, "A");
+
+        memberRepository.save(member);
+
+        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+    }
+
+    @Test
+    void saveMembers(){
+        List<Member> members = List.of(new Member(2L,"B"),
+                new Member(3L,"C"));
+
+        memberRepository.saveAll(members);
+
+        assertThat(memberRepository.findAll().size()).isEqualTo(2);
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteMemberById(){
+        memberRepository.deleteById(2L);
+
+        assertThat(memberRepository.findById(2L).isEmpty()).isTrue();
+    }
+
+
+    @Sql("/insert-members.sql")
+    @Test
+    void deleteAll(){
+        memberRepository.deleteAll();
+
+        assertThat(memberRepository.findAll().size()).isZero();
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void update(){
+
+        Member member = memberRepository.findById(2L).get();
+
+        member.changeName("BC");
+
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
+
+    }
+
+}
